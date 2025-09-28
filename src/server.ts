06ab4347b81ccc10f31c54a1836e7e95cb8013b1
Marcos Antonio <marcost.santana@hotmail.com>
2025-09-27 21:01:23 -0300

    permission url frontend

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,24 @@
 import { buildApp } from './app'
+import cors from '@fastify/cors'
 
 const app = buildApp()
+
+app.register(cors, {
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      process.env.FRONTEND_DEV_URL,
+      process.env.FRONTEND_PROD_URL,
+    ]
+
+    // Em desenvolvimento, `origin` pode ser undefined para requisições do mesmo host (ex: Insomnia)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+      return
+    }
+
+    callback(new Error('Not allowed by CORS'), false)
+  },
+})
 
 app
   .listen({ port: 3333, host: '0.0.0.0' })
